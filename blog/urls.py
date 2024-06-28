e3006d7766ffba5819e8bd7fb65ed8bd0ebb67a6
Ma'ruf Shabonov <shabonov.m@bnpz.local>
2024-06-28 15:17:56 +0500

    add

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, chronicle, politics, leader
 
 urlpatterns = [
-    path("", index)
+    path("", index, name='index'),
+    path("chronicle/", chronicle, name="chronicle"),
+    path("politics/", politics, name="politics"),
+    path("leader/", leader, name="leader"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def chronicle(request):
+    return render(request, 'chronicle.html')
+
+def politics(request):
+    return render(request, 'politics.html')
+
+def leader(request):
+    return render(request, 'leader.html')
